18e0c812f4f90f67dbad19594ef46641486d7372
Denys Demydenko <8544891@stud.nau.edu.ua>
2026-03-25 20:43:10 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculator_project",
+    version="0.1.0",
+    author="Your Team Name",
+    packages=find_packages(),
+    install_requires=[
+        "pytest==7.4.0",
+    ],
+    python_requires=">=3.8",
+)
